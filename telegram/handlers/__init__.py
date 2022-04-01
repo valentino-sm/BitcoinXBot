10047e9c7371662d9767dde44f7980ed9816c909
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2022-04-01 21:21:37 +0500

    refactor: command info

--- telegram/handlers/__init__.py
+++ telegram/handlers/__init__.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher
-from aiogram.dispatcher.filters import CommandStart, CommandHelp
+from aiogram.dispatcher.filters import CommandStart, Command
 
 from . import common
 from .error_handler import error_handler
@@ -12,6 +12,6 @@ def register_errors_handler(dp: Dispatcher):
 
 def register_handlers_base(dp: Dispatcher):
     dp.register_message_handler(common.cmd_start, CommandStart())
-    dp.register_message_handler(common.cmd_info, CommandHelp())
+    dp.register_message_handler(common.cmd_info, Command("info"))
 
-    dp.register_callback_query_handler(common.cq_change_language, cb_start.filter(property="language", value="change"))
+    dp.register_callback_query_handler(common.cq_change_language, cb_start.filter(property="language"))
