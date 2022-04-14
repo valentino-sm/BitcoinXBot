447e04b7dceb89982c38f424d0d6c09975260d61
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2022-04-14 12:56:15 +0500

    fix: reply exceptions handling

--- telegram/utils/reply.py
+++ telegram/utils/reply.py
@@ -23,7 +23,13 @@ async def reply(msg: Union[types.Message, types.CallbackQuery] = None, **kwargs)
                 logger.warning("None for both Message & CallbackQuery provided")
                 await Dispatcher.get_current().bot.send_message(chat_id=User.get_current().id, **kwargs)
         except exceptions.RetryAfter as e:
-            print(f"Target [ID:{User.get_current().id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
+            logger.error(f"Target [ID:{User.get_current().id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
             await asyncio.sleep(e.timeout)
+        except exceptions.ChatNotFound as e:
+            logger.error(e)
+            return
+        except exceptions.BotBlocked as e:
+            logger.error(e)
+            return
         i -= 1
     return r
